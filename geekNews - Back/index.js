29eea5d9decfc82ea67b3e810328f77ee1ea4194
jeandmalves <jeandmalves@gmail.com>
2022-01-21 16:04:43 -0300

    Verificação se o email do usuário já está cadastrado

--- geekNews - Back/index.js
+++ geekNews - Back/index.js
@@ -20,26 +20,37 @@
     server.post('/users', (req, res) => {
         // Cadastra um usuário novo no banco de dados
         const user = req.body
+    
         try {
             const email = user.email
             const password = user.password
             if(email && password){
-                User.create({
-                    email: email,
-                    password: password
-                })
-                return res.json({
-                    "message": "Usuário cadastrado com sucesso"
+                const verificaExisteUser = User.findOne({where: {email: email}}).then((dados) => {
+                    if(dados){
+                        return res.json({
+                            "message": "E-mail já está cadastrado no sistema",
+                            "resultado": false
+                        })
+                    }else{
+                        User.create({
+                            email: email,
+                            password: password
+                        })
+                        return res.json({
+                            "message": "Usuário cadastrado com sucesso",
+                            "resultado": true
+                        })
+                    }
                 })
             }else{
                 throw "Error"
             }
         } catch(error) {
             return res.json({
-                "message": "Usuário não foi cadastrado"
+                "message": "Usuário não foi cadastrado",
+                "resultado": false
             })
-        }
-        
+        } 
     })
 
     server.post('/login', (req, res, next) => {
