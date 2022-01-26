b6db8cadef8014aebc6bf24ec5c48d8b590b0798
jeandmalves <jeandmalves@gmail.com>
2022-01-26 17:32:56 -0300

    Serviço para enviar o email de recuperação feito

--- geekNews - Back/index.js
+++ geekNews - Back/index.js
@@ -3,12 +3,60 @@
     const User = require('./user');
     const cors = require('cors')
 
+    const nodemailer = require("nodemailer")
+    const gmail = "trabalhosifurg@gmail.com"
+    const senha = "#a123456789"
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: gmail,
+            pass: senha
+        }
+    });
+    
     await database.sync();
 
     const express = require('express');
     const server = express();
     server.use(express.json())
     server.use(cors())
+
+    server.post('/recover', (req, res) => {
+        const email_user = req.body.email
+        if(email_user){
+            User.findOne({where: {email: email_user}}).then((dados) => {
+                if(dados){
+                    const mailOptions = {
+                        from: gmail,
+                        to: email_user,
+                        subject: 'Recuperação de senha',
+                        text: `Senha ${dados.password}`
+                    };
+
+                    transporter.sendMail(mailOptions, function(error, info){
+                        if (error) {
+                            return res.json({
+                                "result": error 
+                            })
+                        }
+
+                        return res.json({
+                            "result": true 
+                        })
+                        
+                    })
+                } else{
+                    return res.json({
+                        "result": false
+                    })
+                }
+            })
+        }
+        else{
+            return res.json({"mensagem": "Algum erro ocorreu"})
+        }
+    })
+
 
     server.get('/users', (req, res) => {
         // Retorna os usuários cadastrados no banco de dados
@@ -25,7 +73,7 @@
             const email = user.email
             const password = user.password
             if(email && password){
-                const verificaExisteUser = User.findOne({where: {email: email}}).then((dados) => {
+                User.findOne({where: {email: email}}).then((dados) => {
                     if(dados){
                         return res.json({
                             "message": "E-mail já está cadastrado no sistema",
@@ -53,7 +101,7 @@
         } 
     })
 
-    server.post('/login', (req, res, next) => {
+    server.post('/login', (req, res) => {
         // Verifica se o usuário existe no banco de dados
         const user = req.body
         User.findAll().then((dados) => {
